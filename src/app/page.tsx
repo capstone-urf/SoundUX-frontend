cd2f1e3d89beb4bf45dbfb2a3fc1658662351f8f
ohprettyhak <haklee.googl@gmail.com>
2024-06-10 00:40:50 +0900

    fix: Audio fetch 링크 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home(): ReactElement {
           사용 되고 있습니다
         </h2>
         <Audio
-          audioUrl="https://www.musicplug.co.kr/preview/audio/1001741409.mp3"
+          audioUrl="https://www.musicplug.co.kr/preview/1001741409.mp3"
           width="100%"
           height="18px"
         />
